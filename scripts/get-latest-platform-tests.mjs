f16741228d945e86345b2fab84ae400ade1681b0
Domenic Denicola <d@domenic.me>
2026-01-20 12:15:17 +0900

    Various modernizations
    Includes upgrading `whatwg-mimetype` to v5.0.0 and adding auto-publication.

--- scripts/get-latest-platform-tests.mjs
+++ scripts/get-latest-platform-tests.mjs
@@ -1,11 +1,9 @@
-"use strict";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
 
 if (process.env.NO_UPDATE) {
   process.exit(0);
 }
-
-const path = require("node:path");
-const fs = require("node:fs/promises");
 
 // Pin to specific version, reflecting the spec version in the readme.
 //
@@ -13,24 +11,17 @@ const fs = require("node:fs/promises");
 // 1. Go to https://github.com/web-platform-tests/wpt/tree/master/fetch/data-urls
 // 2. Press "y" on your keyboard to get a permalink
 // 3. Copy the commit hash
-const commitHash = "d9d78543960a04ea8ad8f1aa3c7536b6a9a87d9a";
+const commitHash = "2dc0ad4f330f0b5647d11b00ae7437b668cb8df3";
 
 const urlPrefix = `https://raw.githubusercontent.com/w3c/web-platform-tests/${commitHash}` +
                   `/fetch/data-urls/resources/`;
 
 const files = ["base64.json", "data-urls.json"];
 
-async function main() {
-  await Promise.all(files.map(async file => {
-    const url = urlPrefix + file;
-    const targetFile = path.resolve(__dirname, "..", "test", "web-platform-tests", file);
+await Promise.all(files.map(async file => {
+  const url = urlPrefix + file;
+  const targetFile = path.resolve(import.meta.dirname, "..", "test", "web-platform-tests", file);
 
-    const res = await fetch(url);
-    await fs.writeFile(targetFile, res.body);
-  }));
-}
-
-main().catch(e => {
-  console.error(e.stack);
-  process.exit(1);
-});
+  const res = await fetch(url);
+  await fs.writeFile(targetFile, res.body);
+}));
